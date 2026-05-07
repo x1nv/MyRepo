147b2175878c4134588e5366c950a0928f3513db
x1nv <33998532+x1nv@users.noreply.github.com>
2026-05-08 00:38:47 +0800

    1

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello, World! This is versioned by Release Please." << std::endl;
+    std::cout << "Hello,  World! This is versioned by Release Please." << std::endl;
     return 0;
 }
